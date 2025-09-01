2b98a7722ad30f37093c27451e20c69f06bff812
albertadrianne-bit <albert.adrianne@gmail.com>
2025-09-01 06:35:43 -0700

    Create quote-flow.php
    v2.2.2 - Unified build

---
+++ includes/quote-flow.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined('ABSPATH') ) exit;
+
+add_action('woocommerce_proceed_to_checkout', function(){
+  if (! intval(get_option('annesfs_request_quote_enabled',1)) ) return;
+  echo '<a href="'.esc_url(wc_get_cart_url()).'?annesfs_quote=1" class="button wc-forward annesfs-btn-quote" style="margin-left:8px;">Request a Quote</a>';
+}, 25);
