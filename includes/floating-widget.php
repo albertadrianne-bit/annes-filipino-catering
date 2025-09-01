ed74dc03bdfd01fe80870d39937c4ec61dac220a
albertadrianne-bit <albert.adrianne@gmail.com>
2025-09-01 06:36:22 -0700

    Create floating-widget.php
    v2.2.2 - Unified build

---
+++ includes/floating-widget.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined('ABSPATH') ) exit;
+
+add_action('wp_footer', function(){
+  if ( ! intval(get_option('annesfs_est_show_floating',1)) ) return;
+  echo do_shortcode('[annesfs_guest_estimator]');
+});
