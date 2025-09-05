e95e3f1ee21019e9d49aa40244acc14cf1ffb53e
albertadrianne-bit <albert.adrianne@gmail.com>
2025-09-05 11:11:40 -0700

    Update quickview.php
    v2.2.4-stable: bundles + quick-view + mini-cart polish + quote flow

--- includes/quickview.php
+++ includes/quickview.php
@@ -28,7 +28,7 @@ function annesfs_qv(){
   <div class="qv-head"><div class="qv-title"><?php echo esc_html($product->get_name()); ?></div>
   <div class="qv-price"><?php echo wp_kses_post($product->get_price_html()); ?></div></div>
   <div class="qv-body">
-    <div class="qv-media"><?php echo $product->get_image('woocommerce_single',['style'=>'border-radius:10px;max-width:100%;height:auto;object-fit:cover']); ?></div>
+    <div class="qv-media"><?php echo $product->get_image('woocommerce_single',["style"=>'border-radius:10px;max-width:100%;height:auto;object-fit:cover']); ?></div>
     <div class="qv-form">
     <?php if($product->is_type('variable')){
       $attributes=$product->get_variation_attributes(); $available=$product->get_available_variations();
